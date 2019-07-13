d272710d00095b60f10e0da479ca872dbe55a856
fpestunov <fpestunov@gmail.com>
2019-07-13 22:40:11 +0400

    refactor score method with array sum

--- src/Bowling.php
+++ src/Bowling.php
@@ -3,15 +3,29 @@
 class Bowling
 {
 
-	protected $score = 0;
+	// REFACTOR
 
-    public function roll($roll)
+	// protected $score = 0;
+
+ //    public function roll($roll)
+ //    {
+ //        $this->score += $roll;
+ //    }
+
+ //    public function score()
+ //    {
+ //        return $this->score;
+ //    }
+
+	protected $rolls = [];
+
+    public function roll($pins)
     {
-        $this->score += $roll;
+        $this->rolls[] = $pins;
     }
 
     public function score()
     {
-        return $this->score;
+        return array_sum($this->rolls);
     }
 }
