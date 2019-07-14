7cd611ede6fdb8d1739790fdf65f801389c9c611
fpestunov <fpestunov@gmail.com>
2019-07-14 06:32:14 +0400

    refactor test and method for strike case

--- src/Bowling.php
+++ src/Bowling.php
@@ -19,11 +19,11 @@ class Bowling
 
     	for ($frame=1; $frame <= 10; $frame++) { 
 
-    		if ($this->rolls[$roll] == 10) {
-	    		$score += 10 + $this->rolls[$roll + 1] + $this->rolls[$roll + 2];
+    		if ($this->isStrike($roll)) {
+	    		$score += 10 + $this->strikeBonus($roll);
 	    		$roll += 1;
     		} elseif ($this->isSpare($roll)) {
-	    		$score += 10 + $this->rolls[$roll + 2];
+	    		$score += 10 + $this->spareBonus($roll);
 	    		$roll += 2;
     		} else {
 	    		$score += $this->getDefaultFrameScore($roll);
@@ -35,12 +35,27 @@ class Bowling
     	return $score;
     }
 
-    public function isSpare($roll)
+    private function isStrike($roll)
+    {
+    	return $this->rolls[$roll] == 10;
+    }
+
+    private function strikeBonus($roll)
+    {
+    	return $this->rolls[$roll + 1] + $this->rolls[$roll + 2];
+    }
+
+    private function isSpare($roll)
     {
     	return $this->rolls[$roll] + $this->rolls[$roll + 1] == 10;
     }
 
-    public function getDefaultFrameScore($roll)
+    private function spareBonus($roll)
+    {
+    	return $this->rolls[$roll + 2];
+    }
+
+    private function getDefaultFrameScore($roll)
     {
     	return $this->rolls[$roll] + $this->rolls[$roll + 1];
     }
